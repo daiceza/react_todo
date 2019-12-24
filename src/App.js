7bb5042bf03f0963cdde7b5d148e8139212fd7c5
sase_daiki <sase_daiki@yahoo.co.jp>
2019-12-24 22:19:12 +0900

    2019/12/24todo完了状態を追加

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     super();
     this.state ={
       todos:[
-        {id:1,text:'dataを表示する'},
-        {id:2,text:'簡単な構成を知る'}
+        {id:1,text:'dataを表示する',isDone:false},
+        {id:2,text:'簡単な構成を知る',isDone:false}
       ],
       newTodo:{text:''}
     };
@@ -18,7 +18,7 @@ class App extends Component {
   handleInput =e =>{
     const newId =Math.max.apply(null,this.state.todos.map(t => t.id))+1;
     this.setState({
-      newTodo:{Id:newId, text: e.target.value}
+      newTodo:{Id:newId, text: e.target.value,isDone:false}
     });
   };
   createNewToDoItem =()=>{
@@ -35,15 +35,34 @@ class App extends Component {
       todos:todos.filter((todo)=>todo.id !== item.id)
     }));
   };
+  toggleCheckBox = item =>{
+    const tempTodos = this.state.todos.map(todo=>{
+      if(todo === item){
+        todo.isDone = !todo.isDone;
+      }
+      return todo
+    });
+    this.setState(({todos})=>({
+      todos:tempTodos
+    }));
+  };
   render(){
+    let remaining = this.state.todos.filter(function(todo){
+      return !todo.isDone;
+    });
     return (
       <div className="App">
+        <h1>
+          My Todos
+    <span>({remaining.length}/{this.state.todos.length})</span>
+        </h1>
         <ul>
           {this.state.todos.map((item,key)=>{
           return(
             <ToDoItem
               key={key}
               text={item.text}
+              toggleCheckBox={this.toggleCheckBox.bind(this,item)}
               deleteTodo={this.deleteTodo.bind(this,item)}
             />
           )
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -4,6 +4,7 @@ class ToDoItem extends Component{
   render(){
     return(
       <li>
+        <input type="checkbox" onChange={this.props.toggleCheckBox}/>
         <span>{this.props.text}</span>
         <span onClick={this.props.deleteTodo}>[x]</span>
       </li>
